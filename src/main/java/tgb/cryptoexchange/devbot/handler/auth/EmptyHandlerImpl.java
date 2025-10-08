35b302b270a1e58cf0ba293e1ac859e0698e647f
barilo <e.barilo@isida.by>
2025-10-08 15:27:44 +0300

    up tg-common version, изменена логика EmptyHandler

--- src/main/java/tgb/cryptoexchange/devbot/handler/auth/EmptyHandlerImpl.java
+++ src/main/java/tgb/cryptoexchange/devbot/handler/auth/EmptyHandlerImpl.java
@@ -1,14 +1,13 @@
 package tgb.cryptoexchange.devbot.handler.auth;
 
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.botapimethods.BotApiMethodMessage;
 import tgb.cryptoexchange.tgcommon.handler.EmptyHandler;
 
 @Service
 public class EmptyHandlerImpl implements EmptyHandler {
 
     @Override
-    public BotApiMethodMessage getEmptyMessage(Long chatId) {
-        return null;
+    public void handle(Long chatId) {
+        // отсутствует обработка
     }
 }
